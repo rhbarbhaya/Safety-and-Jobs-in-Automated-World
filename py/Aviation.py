b6ea99abf2970d3339698f768994acf6c017d4cd
Rushabh Barbhaya <rush@Rushabhs-MacBook-Pro.local>
2022-04-08 23:30:15 -0600

    init plots

--- py/Aviation.py
+++ py/Aviation.py
@@ -7,12 +7,17 @@ from sqlite3 import DataError
 import pandas as pd
 pd.set_option('mode.chained_assignment', None)
 
-class aviation:
+class Aviation:
+    """Get the aviation data in the format and the scope and required fo the project
+    The Project is to get the number of people injured by aviation accidents and the time
+    to make a statement on aircraft industry and it's automation againts safety
+    """
     def get_scope(self, scope_directory="./data"):
-        """_summary_
+        """get the whole scope of the dataset that is required for the analysis
 
         Args:
-            scope_directory (str, optional): get the data folder directory. Defaults to "./data".
+            scope_directory (str, optional): get the data folder directory.
+            Defaults to "./data".
 
         Returns:
             pandas.DataFrame: scoped dataframe for all the files in scope
@@ -45,20 +50,24 @@ class aviation:
                         ]
                         master_data = pd.concat([master_data, data_f])
             print("\n\n")
-            print(f"Data read complete --- Total {master_data.shape[0]} records across {master_data.shape[1]} columns")
+            print(f"Data read complete --- Total {master_data.shape[0]} \
+                records across {master_data.shape[1]} columns")
             return master_data
-        except:
-            raise DataError("Error reading data, BREAKING ISSUE")
-        
+        except Exception as error:
+            raise DataError("Error reading data, BREAKING ISSUE") from error
 
 
-    def country_scope(self, _data_, country=['USA']): 
-        """Scope the dataset based on a country filer, taken in a list argument of the country code that is in dataset.
+
+    def country_scope(self, _data_, country=['USA']):
+        """Scope the dataset based on a country filer, taken in a list argument of
+        the country code that is in dataset.
         Please refer to the ct_iaids dataset for scope defiantion
 
         Args:
-            _data_ (pandas.DataFrame): get the master DataFrame that is used for the scope mapping
-            country (list, optional): list arguent for all the countries required for the scope. Defaults to ['USA'].
+            _data_ (pandas.DataFrame): get the master DataFrame that is used for
+            the scope mapping
+            country (list, optional): list arguent for all the countries required
+            for the scope. Defaults to ['USA'].
 
         Returns:
             pandas.DataFrame: returns the countries that are in scope
@@ -68,19 +77,23 @@ class aviation:
             print(f"Scoping data to USA --- Removed {_data_.shape[0] - new_data.shape[0]} records")
             print(f"Total records --- {new_data.shape[0]} across {new_data.shape[1]} columns")
             return new_data
-        except Exception as e:
-            print(f"Error in processing records, NON_BREAKING")
-            print(f"Error\n{e}")
+        except Exception as error:
+            print("Error in processing records, NON_BREAKING")
+            print(f"Error\n{error}")
             return _data_
 
 
     def state_scope(self, _data_, states=[]):
-        """Scope the dataset based on the states in a country filer, taken in a list argument of the states 
-        in a country code that is in dataset. Please refer to the ct_iaids dataset for scope defiantion
+        """Scope the dataset based on the states in a country filer, taken in a list
+        argument of the states
+        in a country code that is in dataset. Please refer to the ct_iaids dataset
+        for scope defiantion
 
         Args:
-            _data_ (pandas.DataFrame): Get the master DataFrame that is used for the scope mapping
-            states (list, optional): all the args for state of the country that is in scope. Defaults to [].
+            _data_ (pandas.DataFrame): Get the master DataFrame that is used for
+            the scope mapping
+            states (list, optional): all the args for state of the country that
+            is in scope. Defaults to [].
 
         Returns:
             pandas.DataFrame: returns the states of the country that are in scope
@@ -90,9 +103,9 @@ class aviation:
             print(f"Scoping data to USA --- Removed {_data_.shape[0] - new_data.shape[0]} records")
             print(f"Total records --- {new_data.shape[0]} across {new_data.shape[1]} columns")
             return new_data
-        except Exception as e:
+        except Exception as error:
             print(f"Error in processing records, NON_BREAKING")
-            print(f"Error\n{e}")
+            print(f"Error\n{error}")
             return _data_
 
 
@@ -101,7 +114,8 @@ class aviation:
 
         Args:
             _data_ (pandas.DataFrame): The input data set from any previous step
-            key_column (list, optional): primary keys in the dataset. Defaults to ['ev_id', 'ntsb_no'].
+            key_column (list, optional): primary keys in the dataset.
+            Defaults to ['ev_id', 'ntsb_no'].
 
         Returns:
             pandas.DataFrame: DataFrame with duplicates removed
@@ -111,22 +125,31 @@ class aviation:
             print(f"{_data_.shape[0] - new_data.shape[0]} dupes found and removed")
             print(f"Total records --- {new_data.shape[0]} with {new_data.shape[1]} columns")
             return new_data
-        except Exception as e:
+        except Exception as error:
             print(f"Error in processing records, NON_BREAKING")
-            print(f"Error\n{e}")
+            print(f"Error\n{error}")
             return _data_
 
 
-    def total_injuries(self, _data_, ground_scope=['inj_f_grnd', 'inj_m_grnd', 'inj_s_grnd'], flight_scope=['inj_tot_f', 'inj_tot_m', 'inj_tot_s']):
-        """Calculate the injuries count for ground, flight and overall. scope is pre-fixed, can be changed on parameters
+    def total_injuries(
+        self,
+        _data_,
+        ground_scope=['inj_f_grnd', 'inj_m_grnd', 'inj_s_grnd'],
+        flight_scope=['inj_tot_f', 'inj_tot_m', 'inj_tot_s']
+        ):
+        """Calculate the injuries count for ground, flight and overall. scope is pre-fixed,
+        can be changed on parameters
 
         Args:
             _data_ (pandas.DataFrame): scoped input for ETL
-            ground_scope (list, optional): scope for ground injuries. Defaults to ['inj_f_grnd', 'inj_m_grnd', 'inj_s_grnd'].
-            flight_scope (list, optional): scope for flight injuries. Defaults to ['inj_tot_f', 'inj_tot_m', 'inj_tot_s'].
+            ground_scope (list, optional): scope for ground injuries.
+            Defaults to ['inj_f_grnd', 'inj_m_grnd', 'inj_s_grnd'].
+            flight_scope (list, optional): scope for flight injuries.
+            Defaults to ['inj_tot_f', 'inj_tot_m', 'inj_tot_s'].
 
         Returns:
-            pandas.DataFrame: The same dataset with 3 additional columns, showing the ground, flight and total injuries
+            pandas.DataFrame: The same dataset with 3 additional columns, showing the ground,
+            flight and total injuries
         """
         scope = ground_scope+flight_scope
         for key in scope:
@@ -157,14 +180,17 @@ class aviation:
         return
 
 
-    # def main(self):
-    """Main block to control other functions of the code.
-    """
-    setup_init()
-    master_data = get_scope()
-    country_scoped = country_scope(master_data)
-    dupes_removed= remove_dupes(country_scoped)
-    master = total_injuries(dupes_removed)
-    print(f"dataset shape --- {master.shape}")
-    print("-"*65)
-    # return master
+    def get_master_set(self):
+        """_summary_
+
+        Returns:
+            _type_: _description_
+        """
+        self.setup_init()
+        master_data = self.get_scope()
+        country_scoped = self.country_scope(master_data)
+        dupes_removed= self.remove_dupes(country_scoped)
+        master = self.total_injuries(dupes_removed)
+        print(f"dataset shape --- {master.shape}")
+        print("-"*65)
+        return master
--- py/aviation_images.py
+++ py/aviation_images.py
@@ -1,4 +1,42 @@
-import aviation_main
+from pydoc import describe
+from Aviation import Aviation
+import matplotlib.pyplot as plt
+import pandas as pd
 
-master = aviation_main()
-print(master.shape)
+
+c = Aviation()
+master = c.get_master_set()
+print(master.head().to_string())
+# print(master.describe())
+
+df = master.copy()
+df['ev_month'] = df['ev_month'].astype(int)
+df['ev_year'] = df['ev_year'].astype(int)
+df = df[['ev_year', 'ev_month', 'ground_injuries', 'total_injuries']]
+df = df.groupby(by=['ev_year'], sort=True, as_index=False).sum()
+# df['date'] = df['ev_month'].map(str)+ '-' +df['ev_year'].map(str)
+# df['date'] = pd.to_datetime(df['date'], format='%m-%Y').dt.strftime('%m-%Y')
+print(df)
+# fig, ax = plt.subplots()
+# plt.plot_date(df['ev_year'], df['ground_injuries'], color='blue')
+# plt.plot_date(df['ev_year'], df['total_injuries'], color='green')
+# plt.show()
+
+#Plot configuration
+fig, ax1 = plt.subplots()
+
+#Plot 1 -- Ground Injuries
+ax1.set_xlabel('Year')
+ax1.set_ylabel('Ground Injuries (sum)', color='blue')
+ax1.scatter(df['ev_year'], df['ground_injuries'], color='blue', label='Ground Injuries')
+ax1.tick_params(axis='y', labelcolor='blue')
+
+#Plot 2 -- Flight Injuries
+ax2 = ax1.twinx()
+ax2.set_ylabel('Flight Injuries (sum)', color='green')
+ax2.scatter(df['ev_year'], df['total_injuries'], color='green', label='Flight Injuries')
+ax2.tick_params(axis='y', labelcolor='green')
+
+#Legends
+plt.legend()
+plt.show()
